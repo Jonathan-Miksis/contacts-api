ac7959f7303a89b3971d9e704607a45a190790fd
Jonathan-Miksis <jmiksis13@gmail.com>
2021-10-20 19:32:23 -0400

    add create route/action for new contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,4 +11,18 @@ class ContactsController < ApplicationController
     contact_last = Contact.last
     render json: {contact: contact_last}
   end
+
+  def create
+    contact = Contact.new(
+      first_name: "Jon",
+      last_name: "Miksis",
+      email: "jmiksis13@gmail.com",
+      phone_number: "603-812-7478",
+      latitude: 7.392730,
+      longitude: 83.482639
+    )
+    contact.save
+    render json: contact.as_json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "/contacts/:id" => "contacts#show"
   get "/contacts" => "contacts#index"
   get "/last_contact", controller: "contacts", action: "last_contact"
+
+  post "/contacts" => "contacts#create"
 end
