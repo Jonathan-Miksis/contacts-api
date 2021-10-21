f0d43c6e706757ee8292fedfe11c62e912d0c499
Jonathan-Miksis <jmiksis13@gmail.com>
2021-10-20 20:50:06 -0400

    add geocoder and hardcode address in controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,13 +10,16 @@ class ContactsController < ApplicationController
   end
 
   def create
+    results = Geocoder.search("Paris")
+    geo_latitude = results.first.coordinates[0]
+    geo_longitude = results.first.coordinates[1]
     contact = Contact.new(
       first_name: params[:first_name],
       last_name: params[:last_name], 
       email: params[:email],
       phone_number: params[:phone_number],
-      latitude: params[:latitude],
-      longitude: params[:longitude]
+      latitude: geo_latitude,
+      longitude: geo_longitude
     )
     contact.save
     render json: contact.as_json
